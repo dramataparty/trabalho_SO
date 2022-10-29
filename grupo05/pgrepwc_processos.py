88638042c17a1b42242eb7d9b466f7cf1fff39bb
dramataparty <101320231+dramataparty@users.noreply.github.com>
2022-10-29 18:27:31 +0100

    Add files via upload

---
+++ grupo05/pgrepwc_processos.py
@@ -0,0 +1,13 @@
+### Grupo: SO-TI-XX
+### Aluno 1: Nome Apelido (fcXXXX)
+### Aluno 2: Nome Apelido (fcXXXX)
+### Aluno 3: Nome Apelido (fcXXXX)
+
+import sys
+
+def main(args):
+    print('Programa: pgrepwc_processos.py')
+    print('Argumentos: ',args)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
